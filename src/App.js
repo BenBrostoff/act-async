fb87ee34b7e91ddd6a431d2214ae4adcd04967c2
Ben Brostoff <ben.brostoff@gmail.com>
2019-08-10 19:35:04 -0400

    Reproduce issue

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export const App = () => {
+  const [data, setData] = useState("unloaded value");
+  const simulatedFetch = async () => {
+    setData("loading value");
+    const fetchedValue = await Promise.resolve('fetched value');
+    await new Promise(res => {
+      setData(fetchedValue);
+      res();
+    });
+  };
+
+  useEffect(() => {
+    simulatedFetch();
+  }, [data]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>{data}</h2>
     </div>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+let el;
+beforeEach(() => {
+  el = document.createElement("div");  
+})
+
+it("should render", async () => {
+  act(() => {
+    ReactDOM.render(<App/>, el);
+  })
+  expect(el.innerHTML).toContain('loading value');
+
+  await act(async () => {
+    await new Promise(res => res());
+  });
+  expect(el.innerHTML).toContain('fetched value');
 });
